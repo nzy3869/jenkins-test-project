d6a6aa586fba306cacb2e30d6b0e0026349ff75b
niuzhiyuan <576910506@qq.com>
2019-03-24 18:45:16 +0800

    【jenkins测试】初始版本

---
+++ src/main/java/top/happybull/jenkins/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package top.happybull.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello jenkins!";
+    }
+}
